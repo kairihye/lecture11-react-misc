a5880894ed1d64856020f198b39ff8c0fbcb5fec
kairihye <hyei@uw.edu>
2016-11-09 11:56:37 -0800

    add barking stuff

--- binding-demo.js
+++ binding-demo.js
@@ -1,3 +1,4 @@
+
 // An object representing a Dog
 var fido = {
   name: "Fido",
@@ -21,14 +22,14 @@ fido.bark( /*this = fido*/ ); //=> "Fido woofs"
 console.log('***This is Spot barking***');
 spot.bark( /*this = spot*/ ); //=> "Spot yips"
 
-// console.log("***This is Fido using Spot's bark***");
-// fido.bark = spot.bark; //assign the function
-// fido.bark( /*this = fido*/) //=> "fido yips"
+console.log("***This is Fido using Spot's bark***");
+fido.bark = spot.bark; //assign the function
+fido.bark( /*this = fido*/) //=> "fido yips"
 
 
-// console.log("***This is Fido using Spot's BOUND bark***");
-// fido.bark = spot.bark.bind(spot); //assign the function, but bind "spot" to the this
-// fido.bark( /*this = fido*/) //=> "Spot yips"
+console.log("***This is Fido using Spot's BOUND bark***");
+fido.bark = spot.bark.bind(spot); //assign the function, but bind "spot" to the this
+fido.bark( /*this = spot*/) //=> "Spot yips"
 //It's a ventriloquist dog!
 
 
